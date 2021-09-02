8bc76a3d12fad73722eb28dcefcd85a11a365de3
Carlos Clemente Zazueta Gonzalez <carlos.zazueta@unosquare.com>
2021-09-02 14:58:57 -0500

    My first commit

---
+++ CSharpTopics/CSharpTopics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTopics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
