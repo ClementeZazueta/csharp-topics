51c5b692a9f03352354c3d677420f282cbaa3995
Carlos Clemente Zazueta Gonzalez <carlos.zazueta@unosquare.com>
2021-09-03 17:49:17 -0500

    minor changes

--- CSharpTopics/CSharpTopics/Excercises.cs
+++ CSharpTopics/CSharpTopics/Excercises.cs
@@ -3,10 +3,6 @@ using ExtensionMethods;
 using GenericsClasses;
 using Interfaces.Npcs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpTopics
 {
